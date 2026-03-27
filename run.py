c3eee35cee2c45c46436ac5f1fb66d528ffc2a0e
linegeek <10746488+linegeek@users.noreply.github.com>
2026-03-27 15:20:05 -0500

    Add port

--- run.py
+++ run.py
@@ -1,5 +1,7 @@
 import uvicorn
 
+from config import PORT
+
 if __name__ == "__main__":
-    uvicorn.run("app:app", host="127.0.0.1", port=8000, reload=True)
+    uvicorn.run("app:app", host="127.0.0.1", port=PORT, reload=True)
 
