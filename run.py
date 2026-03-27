24f96ec6b2e9775cbdeb862fe46ce85f7b70e335
linegeek <10746488+linegeek@users.noreply.github.com>
2026-03-27 15:26:32 -0500

    Fix a bug

--- run.py
+++ run.py
@@ -3,5 +3,5 @@ import uvicorn
 from config import PORT
 
 if __name__ == "__main__":
-    uvicorn.run("app:app", host="127.0.0.1", port=PORT, reload=True)
+    uvicorn.run("app:app", host="127.0.0.1", port=int(PORT), reload=True)
 
